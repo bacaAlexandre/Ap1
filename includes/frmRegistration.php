a68b822d491be49e035627018c17fdbd23203b0b
Alexandre Baca <alexandre.baca@viacesi.fr>
2018-03-12 16:35:41 +0100

    modification formulaire récupération donnée entrée

--- includes/frmRegistration.php
+++ includes/frmRegistration.php
@@ -1,15 +1,18 @@
+
+
+
 <form action="#" method="post">
     <div>
         <label for="name">Nom : </label>
-        <input type="text" id="name"  name="name" />
+        <input type="text" id="name"  name="name" value="<?php if(isset($name)) echo $name; ?>"/>
     </div>
     <div>
         <label for="firstName">Prénom : </label>
-        <input type="text" id="firstName" name="firstName" />
+        <input type="text" id="firstName" name="firstName" value="<?php if(isset($firstName)) echo $firstName; ?>"/>
     </div>
     <div>
         <label for="mail">e-mail : </label>
-        <input type="text" id="mail" name="mail" />
+        <input type="text" id="mail" name="mail" value="<?php if(isset($mail)) echo $mail; ?>" />
     </div>
     <div>
         <label for="password">Mot de passe : </label>
