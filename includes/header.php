045fa6a8fd02035f74f69a981b030a455b08d67c
Alexandre Baca <alexandre.baca@viacesi.fr>
2018-03-12 14:06:03 +0100

    Page header

---
+++ includes/header.php
@@ -0,0 +1,9 @@
+<header>
+    <nav>
+        <ul>
+            <li><a href="index.php?page=home">Home</a></li>
+            <li><a href="index.php?page=registration">Registration</a></li>
+            <li><a href="index.php?page=login">Login</a></li>
+        </ul>
+    </nav>
+</header>
