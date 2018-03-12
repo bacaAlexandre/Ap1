b062346341ca285223c4a4c5f6c49196cd4ddfde
Alexandre Baca <alexandre.baca@viacesi.fr>
2018-03-12 15:09:03 +0100

    inclusion automatique des .inc.php

--- index.php
+++ index.php
@@ -3,9 +3,17 @@
 include "./includes/header.php";
 
 
+//$page = (isset($_GET["page"])&& $_GET != "")?  $page = $_GET['page']: $page = "home";
+$page = $_GET['page'] ?? "";
 
+$page = "./includes/" . $page . ".inc.php";
 
+$files = glob("./includes/*.inc.php");
 
+if(in_array($page, $files))
+    include $page;
+else
+    include "./includes/home.inc.php";
 
 
 
