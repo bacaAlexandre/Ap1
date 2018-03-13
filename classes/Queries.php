1cdfe067b425c6371c5e0f70539c11f41cf311ef
Alexandre Baca <alexandre.baca@viacesi.fr>
2018-03-13 14:46:32 +0100

    ajout class queries

---
+++ classes/Queries.php
@@ -0,0 +1,31 @@
+<?php
+
+
+class Queries
+{
+    // DSN: Data Source Name
+    private $dsn ="mysql:dbname=ap;host=localhost;charset=utf8";
+    private $user= "ap";
+    private $password = "ap";
+    private $db;
+
+    public function __construct()
+    {
+        try{
+            $this-> db = new PDO($this -> dsn, $this -> user, $this -> password);
+            $this -> db -> setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+        }
+        catch (PDOException $e){
+            Log::logWrite($e -> getMessage());
+        }
+    }
+
+    public function insert($sql){
+        return $this -> db -> exec();
+    }
+
+    public function __destruct()
+    {
+        unset($this -> db);
+    }
+}
