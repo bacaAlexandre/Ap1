d6de7cb9aaf1e4e280e9ebfa37a50a514275c826
Alexandre Baca <alexandre.baca@viacesi.fr>
2018-03-14 15:23:54 +0100

    ajout formulaire de login

---
+++ includes/frmLogin.php
@@ -0,0 +1,13 @@
+<form action="#" method="post">
+    <div>
+        <label for="mail">Mail : </label>
+        <input type="text" id="mail"  name="mail" value=""/>
+    </div>
+    <div>
+        <label for="password">Password : </label>
+        <input type="text" id="password"  name="password" value=""/>
+    </div>
+    <div>
+        <input type="submit" value="Valider" />
+    </div>
+</form>
