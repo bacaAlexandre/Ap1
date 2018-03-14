621a766ea5d368f6aaeb4f213ed3be3c60c99eba
Alexandre Baca <alexandre.baca@viacesi.fr>
2018-03-14 15:23:10 +0100

    créaction class login

---
+++ classes/Login.php
@@ -0,0 +1,27 @@
+<?php
+
+
+class Login
+{
+    public static function checkLogin(String $mail, String $password)
+    {
+        $rec = new Queries();
+        $password = sha1($password);
+
+        $sql = "SELECT id_users, usenom, useprenom
+                FROM t_users
+                WHERE usemail ='".$mail."' AND usepassword = '".$password."'";
+
+        $reponse = $rec->select($sql)->fetch();
+
+        if($reponse != null){
+            $_SESSION['id'] = $reponse -> id_users;
+            $_SESSION['nom'] = $reponse -> usenom;
+            $_SESSION['prenom'] = $reponse -> useprenom;
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+}
