e29f836b908d8e64a803f098156c7f9c535e7a31
Alexandre Baca <alexandre.baca@viacesi.fr>
2018-03-14 09:22:48 +0100

    modification timezone et fonction static call page

--- index.php
+++ index.php
@@ -1,21 +1,35 @@
 <?php
+ini_set("smtp_port",1025);
 
-include "./includes/header.php";
+date_default_timezone_set("Europe/Paris");
+
+include "./functions/classAutoLoader.php";
+spl_autoload_register('classAutoLoader');
+?>
+
+<!DOCTYPE html>
+<html lang=""fr-FR>
+<head>
+    <meta charset="utf-8"/>
+    <title>CESI AP - Blog/e-commerce</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+
+<body>
+    <div class="container">
+        <?php
+        include "./includes/header.php";
+
+        CallPage::display( $_GET['page'] ?? "");
+
+        include "./includes/footer.php";
+
+        ?>
+    </div>
+</body>
+
+</html>
 
 
-//$page = (isset($_GET["page"])&& $_GET != "")?  $page = $_GET['page']: $page = "home";
-$page = $_GET['page'] ?? "";
-
-$page = "./includes/" . $page . ".inc.php";
-
-$files = glob("./includes/*.inc.php");
-
-if(in_array($page, $files))
-    include $page;
-else
-    include "./includes/home.inc.php";
 
 
-
-
-include "./includes/footer.php";
