d64406b8a0fa38ad71d416ce2502a4c46ab366c2
Alexandre Baca <alexandre.baca@viacesi.fr>
2018-03-15 12:13:27 +0100

    ajout vérification du mail

--- classes/Login.php
+++ classes/Login.php
@@ -7,21 +7,26 @@ class Login
     {
         $rec = new Queries();
         $password = sha1($password);
-
-        $sql = "SELECT id_users, usenom, useprenom
+        if(filter_var($mail,FILTER_VALIDATE_EMAIL)){
+            $sql = "SELECT id_users, usenom, useprenom
                 FROM t_users
                 WHERE usemail ='".$mail."' AND usepassword = '".$password."'";
 
-        $reponse = $rec->select($sql)->fetch();
+            $reponse = $rec->select($sql)->fetch();
 
-        if($reponse != null){
-            $_SESSION['id'] = $reponse -> id_users;
-            $_SESSION['nom'] = $reponse -> usenom;
-            $_SESSION['prenom'] = $reponse -> useprenom;
-            return true;
+            if($reponse != null){
+                $_SESSION['id'] = $reponse -> id_users;
+                $_SESSION['nom'] = $reponse -> usenom;
+                $_SESSION['prenom'] = $reponse -> useprenom;
+                return true;
+            }else{
+                return false;
+            }
         }else{
             return false;
         }
+
+
 
     }
 }
