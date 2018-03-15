200660a6535276dcb1af689c04b24f4c57ae574c
Alexandre Baca <alexandre.baca@viacesi.fr>
2018-03-15 12:15:04 +0100

    modification formulaire de connexion , utilisation classe formulaire et redirection

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -1,9 +1,27 @@
 <?php
 
 
-include "./includes/frmLogin.php";
+//include "./includes/frmLogin.php";
+
+$array = array(
+    "usemail" => array( "value" => "", "name" => "mail", "type" => "text", "nomChamp" => "E-mail"),
+    "usepassword" => array( "value" => "", "name" => "password", "type" => "text", "nomChamp" => "Mot de passe"),
+    "submit" => array( "value" => "Envoyer", "name" => "", "type" => "submit", "nomChamp" => ""),
+);
+$form = array(
+    "action" => "#",
+    "methode" => "post",
+);
+Formulaire::formulaireCreated($array, $form);
+
+
 
 if(isset($_POST['mail']) && $_POST['mail'] != "" && isset($_POST['password']) && $_POST['password']){
-    Login::checkLogin($_POST['mail'], $_POST['password']);
+    if(Login::checkLogin($_POST['mail'], $_POST['password'])){
+        Redirection::redirect('index.php?page=home');
+    }else{
+        echo "Erreur";
+    }
 
 }
+
