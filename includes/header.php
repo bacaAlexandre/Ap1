ac9f9384e857e951b4860720bc4616a57bfd3d97
Alexandre Baca <alexandre.baca@viacesi.fr>
2018-03-15 12:26:23 +0100

    modification menu

--- includes/header.php
+++ includes/header.php
@@ -2,8 +2,22 @@
     <nav>
         <ul>
             <li><a href="index.php?page=home">Home</a></li>
-            <li><a href="index.php?page=registration">Registration</a></li>
-            <li><a href="index.php?page=login">Login</a></li>
+
+            <?php
+                if(!isset($_SESSION['nom'])){
+                    echo "<li><a href='index.php?page=registration'>Registration</a></li>";
+                    echo "<li><a href='index.php?page=login'>Login</a></li>";
+                }
+            ?>
         </ul>
     </nav>
+    <div>
+        <?php
+        if(isset($_SESSION['nom'])){
+            echo "<p>Bonjour ". $_SESSION['nom']." ".$_SESSION['prenom']."</p>";
+            echo "<p><a href='index.php?page=compte'>compte</a></p>";
+            echo "<p><a href='index.php?page=deconnexion'>deconnexion</a></p>";
+        }
+        ?>
+    </div>
 </header>
