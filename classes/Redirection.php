31ef2714c6bb507e8b0e405e9ebb069c6d295cf4
Alexandre Baca <alexandre.baca@viacesi.fr>
2018-03-15 12:27:01 +0100

    modification classe redirection

--- classes/Redirection.php
+++ classes/Redirection.php
@@ -5,7 +5,7 @@ class Redirection
 {
     public static function redirect($url){
         echo "<script type='text/javascript'>";
-        echo "window.location.replace('http://localhost:81/cour_php/AP1/$url');";
+        echo "window.location.replace('http://localhost:81/cour_php/AP1/index.php?page=$url');";
         echo "</script>";
     }
 }
